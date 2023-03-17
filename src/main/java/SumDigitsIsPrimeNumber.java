dcd783b1a5609b15c46b29cd673f09c12a982d7a
Mohamed BOUHAMED <mohamed.bouhamed@fnacdarty.com>
2023-03-17 10:39:48 +0100

    first solution

--- src/main/java/SumDigitsIsPrimeNumber.java
+++ src/main/java/SumDigitsIsPrimeNumber.java
@@ -1,7 +1,29 @@
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.IntStream.rangeClosed;
 
 public class SumDigitsIsPrimeNumber {
     public List<Integer> calculate(int limit) {
-        return null;
+        return rangeClosed(2, limit)
+                .boxed()
+                .filter(this::sumDigitsIsPrime)
+                .collect(Collectors.toList());
+    }
+
+    private boolean sumDigitsIsPrime(Integer number) {
+        return isPrime(sum(number));
+    }
+
+    private boolean isPrime(int number) {
+        return !IntStream
+                .range(2, number)
+                .boxed()
+                .anyMatch(ele -> number % ele == 0);
+    }
+
+    private int sum(int number) {
+        return number % 10 + number / 10;
     }
 }
