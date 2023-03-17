effb92b9b9c95f12cb86b274906697e983e033d4
Mohamed BOUHAMED <mohamed.bouhamed@fnacdarty.com>
2023-03-17 10:20:05 +0100

    create main class

--- src/test/java/SumDigitsIsPrimeNumberTest.java
+++ src/test/java/SumDigitsIsPrimeNumberTest.java
@@ -1,8 +1,11 @@
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertThat;
+
 
 public class SumDigitsIsPrimeNumberTest {
 
@@ -10,6 +13,6 @@ public class SumDigitsIsPrimeNumberTest {
     public void should_return_all_numbers_below_30_with_a_sum_of_its_digits_is_prime_numbers() throws Exception {
         SumDigitsIsPrimeNumber primeNumbers = new SumDigitsIsPrimeNumber();
         List<Integer> actualPrimeNumbers = primeNumbers.calculate(30);
-        assertThat(actualPrimeNumbers).containsExactly(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        Assertions.assertThat(actualPrimeNumbers).containsExactly(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
     }
 }
