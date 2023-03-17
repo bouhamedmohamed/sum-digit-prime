4b7044f1ef49ed32e1035583211db2cda727b40e
Mohamed BOUHAMED <mohamed.bouhamed@fnacdarty.com>
2023-03-17 11:50:17 +0100

    Removed all files that are in the .gitignore

--- src/test/java/SumDigitsIsPrimeNumberTest.java
+++ src/test/java/SumDigitsIsPrimeNumberTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class SumDigitsIsPrimeNumberTest {
 
     @Test
-    public void should_return_all_numbers_below_30_with_a_sum_of_its_digits_is_prime_numbers() throws Exception {
+    public void should_return_all_numbers_below_30_with_a_sum_of_its_digits_is_a_prime_number() throws Exception {
         SumDigitsIsPrimeNumber primeNumbers = new SumDigitsIsPrimeNumber();
         List<Integer> actualPrimeNumbers = primeNumbers.calculate(30);
         Assertions.assertThat(actualPrimeNumbers).containsExactly(2, 3, 5, 7, 10, 11, 12, 14, 16, 20, 21, 23, 25, 29, 30);
