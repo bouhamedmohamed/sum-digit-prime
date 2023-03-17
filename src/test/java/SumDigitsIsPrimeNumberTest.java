966d7beb3c04055e28eae240db105ab6f5db8f88
Mohamed BOUHAMED <mohamed.bouhamed@fnacdarty.com>
2023-03-17 10:02:59 +0100

    First Test

---
+++ src/test/java/SumDigitsIsPrimeNumberTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+
+public class SumDigitsIsPrimeNumberTest {
+
+    @Test
+    public void should_return_all_numbers_below_30_with_a_sum_of_its_digits_is_prime_numbers() throws Exception {
+        SumDigitsIsPrimeNumber primeNumbers = new SumDigitsIsPrimeNumber();
+        List<Integer> actualPrimeNumbers = primeNumbers.calculate(30);
+        assertThat(actualPrimeNumbers).containsExactly(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+    }
+}
